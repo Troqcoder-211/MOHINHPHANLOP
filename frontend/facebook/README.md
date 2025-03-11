75feeace1f71b24eea393c7b3952ad50af903878
thhn_trroq <133735405+Troqcoder-211@users.noreply.github.com>
2025-03-11 14:38:32 +0700

    first commit

---
+++ frontend/facebook/README.md
@@ -0,0 +1,5 @@
+# React + Vite
+
+## Figma
+
+https://www.figma.com/design/be2unEUq71mbLXPg4oEW9g/Facebook-Web-Redesign---Dark-(Community)?node-id=0-1&m=dev&t=XyVZlLCzSDfz5GWl-1
---
+++ frontend/facebook/src/assets/icons/LibraryIcons.jsx
@@ -0,0 +1,10 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+// import your icons
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faUserGroup } from "@fortawesome/free-solid-svg-icons";
+import { faFilm } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faHouse, faUserGroup, faFilm);
+
+export { faHouse, faUserGroup, faFilm };
